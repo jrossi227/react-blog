17c981abbc5a988778de07c24fd85b2275441425
Jonathan Rossi <jonathan.m.rossi@gmail.com>
2017-03-10 15:18:16 -0800

    changing build params for cloud deployment

--- config.js
+++ config.js
@@ -1,16 +1,12 @@
 var port = process.env.PORT || 9080;
-var host = "localhost";
-var protocol = "http:";
 
 if (typeof window !== 'undefined') {
-	host = window.location.hostname;
-	protocol = window.location.protocol;
 	port = window.location.port;
 }
 
 var config = {
     port: port,
-    baseUrl : protocol + "//" + host + (port != "" ? (":" + port) : ""),
+    baseUrl : typeof window !== 'undefined' ? window.location.origin : "http://localhost:" + port,
     pageTitle: 'React Blog',
     itemsPerPage: 5,
     maxPageButtons: 3
